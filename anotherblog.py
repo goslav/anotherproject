4ea458e931d2d92c7e2089f09517e1c639ad6bea
Slavko <slavko.d.91@gmail.com>
2020-08-27 21:33:15 +0200

    Werkzeug routing error

--- anotherblog.py
+++ anotherblog.py
@@ -1,5 +1,10 @@
-from app import app, db
+#I have removed app from 'import app' snytax.I've added below 'create_app' instead of it.
+from app import create_app, db, cli
 from app.models import User, Post
+
+#ask Jorge about the section below since it wasn't here
+app = create_app()
+cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
